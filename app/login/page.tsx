d85a2092edd74be719e91864dab8616a78fc1cec
Kevin Becerra <kevinbece5@gmail.com>
2025-02-20 15:51:02 -0600

    Update Sign in page

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -19,7 +19,7 @@ export default function LoginPage() {
     <div className="min-h-screen flex justify-center items-start md:items-center p-8">
       <Card className="w-full max-w-sm">
         <CardHeader>
-          <CardTitle className="text-2xl">Register</CardTitle>
+          <CardTitle className="text-2xl">Sign in</CardTitle>
         </CardHeader>
         <CardFooter>
           <form action={loginAction} className="w-full">
